e462924008741233c9711cade1d3d9f060faa786
jayakrishnan007sss <66819674+jayakrishnan007sss@users.noreply.github.com>
2023-01-31 00:13:24 +0530

    Add files via upload
    my first commit

---
+++ Project-1-VIdeo Course Selling Website/script.js
@@ -0,0 +1,18 @@
+var login = document.getElementById("login");
+var loginsec = document.getElementById("loginsection");
+login.addEventListener('click', () => {
+    loginsection.style.display = "block"
+})
+
+
+
+
+var close = document.getElementById("close")
+close.addEventListener('click', () => {
+    loginsection.style.display = "none"
+})
+
+var close = document.getElementById("closed")
+close.addEventListener('click', () => {
+    loginsection.style.display = "none"
+})
